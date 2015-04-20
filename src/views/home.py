96c7cfb08d4b4d1e7c5c6b2838d71ec7d094cf9f
Fernando Oliveira <fernando@fholiveira.com>
2015-04-20 17:43:17 -0300

    Style the home page

--- src/views/home.py
+++ src/views/home.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask.ext.login import current_user
 
 
 home = Blueprint('', __name__)
@@ -6,4 +7,7 @@ home = Blueprint('', __name__)
 
 @home.route('/', methods=['GET'])
 def index():
+    if current_user.is_authenticated():
+        return render_template('feed.html')
+
     return render_template('home.html')
